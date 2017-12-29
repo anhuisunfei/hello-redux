3cb6b734c2dec7560394d1901de8a098b1048171
anhuisunfei <anhuisunfei@github.com>
2017-12-29 18:16:23 +0800

    like

--- src/PostBody.js
+++ src/PostBody.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react'
 import store from './redux/store'
 import { connect } from 'react-redux'
 class PostBody extends Component {
-  render () {
-    let thisComments=this.props.comments.filter(item=>(
-      item.post===this.props.postId
-    ))
-    return <div className="post-body">
+  handleLike = () => {
+    this.props.dispatch({ type: 'LIKE', postId: this.props.postId })
+  }
+
+  render() {
+    let thisComments = this.props.comments.filter(
+      item => item.post === this.props.postId
+    )
+    let thisPost = this.props.posts.find(item => item._id === this.props.postId)
+    return (
+      <div className="post-body">
+        <div className="like-num" onClick={this.handleLike}>
+          {thisPost.like} 赞
+        </div>
         <div className="comment-num"> {thisComments.length}条评论</div>
       </div>
+    )
   }
 }
 
-const mapStateToProps = (state)=>({
-  comments:state.comments
+const mapStateToProps = state => ({
+  comments: state.comments,
+  posts: state.posts
 })
-export default connect(mapStateToProps)(PostBody) 
+export default connect(mapStateToProps)(PostBody)
--- src/redux/reducer/posts.js
+++ src/redux/reducer/posts.js
@@ -1,17 +1,25 @@
 let posts = [
   {
-    _id: 1,
-    title: 'Hello React-Router'
+    _id: '1',
+    title: 'Hello React-Router',
+    like: 1
   },
   {
-    _id: 2,
-    title: 'Hello Redux'
+    _id: '2',
+    title: 'Hello Redux',
+    like: 3
   }
 ]
 export const postReducer = (state = posts, action) => {
   switch (action.type) {
-    case 'ACTION_TYPE':
-      return
+    case 'LIKE':
+      let newState = state.map(item => {
+        if (item._id == action.postId) {
+          return { ...item, like: item.like+1 }
+        }
+        return item
+      })
+      return newState
     default:
       return state
   }
